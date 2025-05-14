581ffae8bb8e240b7034bed322525524fe91eb7b
0xaiman <aimandani9797@gmail.com>
2025-05-15 03:28:58 +0800

    feat: minor product page UI tweaks

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -2,28 +2,58 @@
   <div class="p-6">
     <h1 class="text-2xl font-bold mb-4">Product Details</h1>
 
-    <div v-if="product" class="space-y-2">
-      <h2 class="text-xl font-semibold">{{ product.name }}</h2>
-      <p class="text-gray-700">Description: {{ product.description }}</p>
-      <p class="text-blue-600 font-bold">Price: RM {{ product.price }}</p>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <div>
+        <!-- Image -->
+        <img
+          src="https://images.pexels.com/photos/2872755/pexels-photo-2872755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="Product Image"
+          class="w-full h-full object-cover"
+        />
+      </div>
+      <div>
+        <!-- Product Details -->
+
+        <div v-if="product" class="space-y-2">
+          <h2 class="text-2xl md:text-3xl font-bold">{{ product.name }}</h2>
+          <div class="flex items-center gap-2">
+            <StarRatings
+              v-model="rating"
+              :star-size="24"
+              :number-of-stars="5"
+              star-color="#ff9f1c"
+              inactive-color="#e5e7eb"
+              :disable-click="true"
+            />
+          </div>
+
+          <p class="text-gray-700">Description: {{ product.description }}</p>
+          <p class="text-4xl text-blue-600 font-bold">RM {{ product.price }}</p>
+          <button class="bg-blue-600 text-white px-4 py-2 rounded-md">
+            Add to Cart
+          </button>
+        </div>
+        <div v-else class="text-gray-500">Product not found</div>
+      </div>
     </div>
-    <div v-else class="text-gray-500">Product not found</div>
   </div>
 </template>
 
 <script setup>
-import { useRoute } from 'vue-router';
-import { onMounted } from 'vue';
-import { useProductStore } from '~/stores/productStore';
-import { storeToRefs } from 'pinia';
+import { useRoute } from "vue-router";
+import { onMounted } from "vue";
+import { useProductStore } from "~/stores/productStore";
+import { storeToRefs } from "pinia";
+import StarRatings from "vue3-star-ratings";
 
 definePageMeta({
-  layout: 'default',
-})
+  layout: "default",
+});
 
 const route = useRoute();
 const productStore = useProductStore();
 const { currentProduct: product, loading, error } = storeToRefs(productStore);
+const rating = ref(4.5);
 
 onMounted(() => {
   productStore.fetchProduct(route.params.id);
